57d35c68ff50477e46cba5ccf04b4899bc91ff20
LEONE <leonwahungu@gmail.com>
2023-03-01 16:18:04 +0300

    Placeholder

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hello.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show()
+        })
     }
 }
