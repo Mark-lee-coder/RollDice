ac4917757111fbd8b0fdebe0a2bb0aa9cb14bf09
LEONE <leonwahungu@gmail.com>
2023-03-14 12:52:26 +0300

    Final version of game

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -1,19 +1,40 @@
 package com.example.rolldice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var resultTextView1: TextView
+    private lateinit var resultTextView2: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hello.setOnClickListener(View.OnClickListener {
-            Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show()
+        resultTextView1 = findViewById(R.id.result1)
+        resultTextView2 = findViewById(R.id.result2)
+
+        val rollButton: Button = findViewById(R.id.roll)
+        rollButton.setOnClickListener(View.OnClickListener {
+            rollDice1()
+            rollDice2()
+            rollButton.text = "ROLL AGAIN"
         })
+    }
+
+    private fun rollDice1() {
+        val sides = 6
+        val result = Random.nextInt(1, sides + 1)
+        resultTextView1.text = "You rolled a $result!"
+    }
+
+    private fun rollDice2() {
+        val sides = 6
+        val result = Random.nextInt(1, sides + 1)
+        resultTextView2.text = "You rolled a $result!"
     }
 }
